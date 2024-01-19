f58ce0ca4e071841cf0222b2bfd708ab8f4f99e7
TarasLys <134973755+TarasLys@users.noreply.github.com>
2024-01-19 20:06:29 +0000

    Deploying to gh-pages from @ TarasLys/goit-react-hw-08-phonebook@67f5344a6f89d40361e83fa53e4f1c48f4035479 🚀

---
+++ static/js/115.f86e9036.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[115],{5302:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});n(2791);var c=n.p+"static/media/1.0091dc7c6634a116fdf0.jpg",a="HomePage_img__sfQbn",r=n(184),i=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("div",{style:{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",height:"100vh"},children:(0,r.jsx)("img",{className:a,src:c,alt:""})})})}}}]);
+//# sourceMappingURL=115.f86e9036.chunk.js.map
